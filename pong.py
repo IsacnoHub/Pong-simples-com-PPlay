af4357b21916c324b732f1b757117b9bba7a46fd
ISAC AZEVEDO PINHEIRO <isacazevedo@id.uff.br>
2025-09-16 13:55:21 -0300

    É a mesma coisa
    Nesse arquivo novo os textos são gerados, não desenhados, fora um HUD com pontuação e velocidade da bolinha

--- pong.py
+++ pong.py
@@ -11,10 +11,7 @@ Random dá ao jogo mais possibilidades, mesmo um pouco mais fácil ou mais difí
 
 Dist_da_parede = 50 #Distancia entre os pads e a parede
 
-Ponto_player = 0
-Ponto_maquina = 0
-
-janela= Window(1600,1000) #define o tamanho da janela
+janela= Window(2000,800) #define o tamanho da janela
 janela.set_title("Pong") #coloca nome na janela
 
 bola= Sprite("Bola.png") 
@@ -26,47 +23,61 @@ barra_e.set_position(0 + Dist_da_parede, janela.height/2 - barra_e.height/2)    
 barra_d= Sprite("Pad.png")                                                                                  #   <---
 barra_d.set_position(janela.width - barra_d.width - Dist_da_parede, janela.height/2 - barra_d.height/2)         #   <---
 
-P_jogador = Sprite("Z.png") #Pontos começam com zero
-P_maquina = Sprite("Z.png")
-win = Sprite("Win.png")
-lose = Sprite("Lose.png")
+lista_de_pontos = ['0', '1', '2', '3']
 
-lista_de_pontos = ["Z.png", "U.png", "D.png", "T.png"]
+Ponto_player = 0
+Ponto_maquina = 0
+pts = 0
 
-P_jogador.set_position((janela.width/2)-(P_jogador.width/2)-(150),(janela.height/2)-(P_jogador.height/2)-(200)) #Posições do placar
-P_maquina.set_position((janela.width/2)-(P_maquina.width/2)+(150),(janela.height/2)-(P_maquina.height/2)-(200))
-win.set_position((janela.width/2)-(win.width/2),(janela.height/2)-(win.height/2)-350) #Posição de vitória
-lose.set_position((janela.width/2)-(win.width/2),(janela.height/2)-(win.height/2)-350) #Posição de derrota
+#Variaveis do texto na tela
+size = 20
+size_event = 30
+size_placar = 50
+size_pts = 15
+text = "Espaço para jogar | ESC para sair"  
+text_win = "WINNER"   
+text_lose = "GAME OVER"    
+text_placar_widht = len('0') * size_placar
+text_width = len(text) * (size // 2)
+text_width_win = len(text_win) * (size_event / 2)
+text_width_lose = len(text_lose) * (size_event / 2)
+distan = janela.height*5/6
+distan_placar = janela.height*1/4
+distan_event = janela.height*1/8
+dist_meio = 100
+branco = (255, 255, 255)
+vermelho = (255, 0, 0)
 
-vel_ale_1 = 268
-vel_ale_2 = 273
+teclado = janela.get_keyboard()
+
+vel_ale_1 = 265
+vel_ale_2 = 272
 vel_x = 300                                 # VEL
 vel_y = 300                                 # OCI
 vel_barra_e = 500                           # DA
 vel_barra_d = randint(vel_ale_1, vel_ale_2) # DES
 
-
 encremento_vel = 23 #toda vez que toca um pad
 vel_inicio = vel_x #serve pra voltar a velocidade ao normal dps de ponto
-
-teclado = janela.get_keyboard()
 
 estado = 0 #é pra ser o espaço para começar
 vitoria_ativa = False  # NOVA VARIÁVEL PARA CONTROLAR VITÓRIA
 derrota_ativa = False
 
-while True:
+while True: 
+      
     janela.set_background_color((0,0,0))
+    text_placar_d = lista_de_pontos[Ponto_maquina]
+    text_placar_e = lista_de_pontos[Ponto_player]
 
 #mensagem de vitória
     if Ponto_player == 3:
         vitoria_ativa = True
         Ponto_player = 0
         Ponto_maquina = 0
-        P_jogador = Sprite("Z.png") #Pontos começam com zero
-        P_maquina = Sprite("Z.png")
-        P_jogador.set_position((janela.width/2)-(P_jogador.width/2)-(150),(janela.height/2)-(P_jogador.height/2)-(200)) #Posições do placar
-        P_maquina.set_position((janela.width/2)-(P_maquina.width/2)+(150),(janela.height/2)-(P_maquina.height/2)-(200))
+        P_maquina = janela.draw_text(text_placar_d, (janela.width/2 - text_placar_widht/2 + dist_meio), (distan_placar), size_placar, (branco), "Calibri")
+        P_jogador = janela.draw_text(text_placar_e, (janela.width/2 - text_placar_widht/2 - dist_meio), (distan_placar), size_placar, (branco), "Calibri")
+        janela.draw_text(text_win, (janela.width/2 - text_width_win/2), (distan_event), size_event, (branco), "Calibri")
         estado = 0
         vel_barra_d = randint(vel_ale_1, vel_ale_2)
 
@@ -75,10 +86,9 @@ while True:
         derrota_ativa = True
         Ponto_maquina = 0
         Ponto_player = 0
-        P_jogador = Sprite("Z.png") #Pontos começam com zero
-        P_maquina = Sprite("Z.png")
-        P_jogador.set_position((janela.width/2)-(P_jogador.width/2)-(150),(janela.height/2)-(P_jogador.height/2)-(200)) #Posições do placar
-        P_maquina.set_position((janela.width/2)-(P_maquina.width/2)+(150),(janela.height/2)-(P_maquina.height/2)-(200))
+        P_maquina = janela.draw_text(text_placar_d, (janela.width/2 - text_placar_widht/2 + dist_meio), (distan_placar), size_placar, (branco), "Calibri")
+        P_jogador = janela.draw_text(text_placar_e, (janela.width/2 - text_placar_widht/2 - dist_meio), (distan_placar), size_placar, (branco), "Calibri")
+        janela.draw_text(text_lose, (janela.width/2 - text_width_lose/2), (distan_event), size_event, (branco), "Calibri")
         estado = 0
         vel_barra_d = randint(vel_ale_1, vel_ale_2)
 
@@ -91,18 +101,25 @@ while True:
         bola.x += vel_x * janela.delta_time()    
         bola.y += vel_y * janela.delta_time()
 
+#texto na tela
+    if estado == 0:
+        janela.draw_text(text, (janela.width/2 - text_width/2), (distan), size, (255,255,255), "Calibri")
+    
+
+
 #colisão bola paredes laterais:
     if bola.x <=0 or bola.x + bola.width >= janela.width:
         estado = 0
         #placar
         if bola.x <= 0:
             Ponto_maquina += 1
-            P_maquina = Sprite(lista_de_pontos[Ponto_maquina])
-            P_maquina.set_position((janela.width/2)-(P_maquina.width/2)+(150),(janela.height/2)-(P_maquina.height/2)-(200))
+            pts = 0
+            P_maquina = janela.draw_text(text_placar_d, (janela.width/2 - text_placar_widht/2 + dist_meio), (distan_placar), size_placar, (branco), "Calibri")
+
         else:
             Ponto_player += 1
-            P_jogador = Sprite(lista_de_pontos[Ponto_player])
-            P_jogador.set_position((janela.width/2)-(P_jogador.width/2)-(150),(janela.height/2)-(P_jogador.height/2)-(200))
+            pts = 0
+            P_jogador = janela.draw_text(text_placar_e, (janela.width/2 - text_placar_widht/2 - dist_meio), (distan_placar), size_placar, (branco), "Calibri")
 
         if teclado.key_pressed("space"):
             vitoria_ativa = False
@@ -115,8 +132,6 @@ while True:
         bola.set_position((janela.width/2)-(bola.width/2),(janela.height/2)-(bola.height/2))
         barra_e.set_position(0 + Dist_da_parede, janela.height/2 - barra_e.height/2)
         barra_d.set_position(janela.width - barra_d.width - Dist_da_parede, janela.height/2 - barra_d.height/2)
-        P_jogador.set_position((janela.width/2)-(P_jogador.width/2)-(150),(janela.height/2)-(P_jogador.height/2)-(200))
-        P_maquina.set_position((janela.width/2)-(P_maquina.width/2)+(150),(janela.height/2)-(P_maquina.height/2)-(200))
         vel_x = vel_inicio
 
 #colisão bola paredes base-teto:
@@ -131,12 +146,13 @@ while True:
     if bola.collided(barra_e):
         vel_x*= -1
         bola.x= barra_e.x + barra_e.width
+        pts += 1
 
         #encrementa a velocidade
         if vel_x < 0:
-            vel_x -= encremento_vel * janela.delta_time()
+            vel_x -= encremento_vel
         else:
-            vel_x += encremento_vel * janela.delta_time()
+            vel_x += encremento_vel
 
     if bola.collided(barra_d):
         vel_x*=-1
@@ -144,9 +160,9 @@ while True:
 
         #encrementa a velocidade
         if vel_x < 0:
-            vel_x -= encremento_vel * janela.delta_time()
+            vel_x -= encremento_vel
         else:
-            vel_x += encremento_vel * janela.delta_time()
+            vel_x += encremento_vel 
 
 #barra direita controlado por ia:
     if barra_d.y + barra_d.height/2 < bola.y + bola.height/2:
@@ -182,13 +198,15 @@ while True:
         break
 
 #desenha tudo
+    janela.draw_text(text_placar_d, (janela.width/2 - text_placar_widht/2 + dist_meio), (distan_placar), size_placar, (branco), "Calibri")
+    janela.draw_text(text_placar_e, (janela.width/2 - text_placar_widht/2 - dist_meio), (distan_placar), size_placar, (branco), "Calibri")
+    janela.draw_text(str(pts), 0, 0, size_pts, (branco), "Calibri")
+    janela.draw_text(str(abs(vel_x)), 0, size_pts, size_pts, (branco), "Calibri")
     bola.draw()
     barra_e.draw()
     barra_d.draw()
-    P_jogador.draw()
-    P_maquina.draw()
     if vitoria_ativa:
-        win.draw()
+        janela.draw_text(text_win, (janela.width/2 - text_width_win/2), (distan_event), size_event, (branco), "Calibri", True)#True é pra negrito
     if derrota_ativa:
-        lose.draw()
+        janela.draw_text(text_lose, (janela.width/2 - text_width_lose/2), (distan_event), size_event, (vermelho), "Calibri", True)
     janela.update()
